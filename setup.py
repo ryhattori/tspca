99dc792a5e59d66bf25f47648e3b19dc909ef4ec
Ryoma Hattori <rhattori0204@gmail.com>
2020-12-29 02:11:20 -0800

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tspca',
+    version='1.0',
+    packages=['tspca'],
+    url='http://github.com/ryhattori/tspca',
+    license='',
+    author='Ryoma Hattori',
+    author_email='rhattori0204@gmail.com',
+    description='Targeted subspace principal component analysis (tsPCA)'
+)
